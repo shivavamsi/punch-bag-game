8b73d29c7b0e44d03e1097d385f0c76f102b6869
Shiva Vamsi <shivavamsi.gudivada@gmail.com>
2019-01-09 22:54:07 -0500

    fixed audio errors

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 new Vue({
   el: '#punchbag-app',
   data: {
+    // declare variables
     bagHealth: 100,
     isDead: false,
     exclamation: "",
@@ -19,21 +20,36 @@ new Vue({
   },
   methods: {
     punch: function() {
+      // generate random integer to play audio
       var r = Math.floor(Math.random() * Math.floor(3));
-      var audio = new Audio("../media/punch-" + r + ".wav");
-      audio.play();
 
+      // play punch audio
+      document.getElementById('punch-' + r).play();
+
+      // decrease health by 10 for each punch
       this.bagHealth -= 10;
+
+      // change isDead flag if health is 0
       if (this.bagHealth <= 0)
         this.isDead = true;
 
+      // generate a random number for expression
       var i = Math.floor(Math.random() * Math.floor(10));
 
+      // update the expression
       this.exclamation = this.exclamations[i];
     },
     resetGame: function() {
+      // play restart audio
+      document.getElementById('restart').play();
+
+      // reset bag health
       this.bagHealth = 100;
+
+      // reset isDead flag
       this.isDead = false;
+
+      // reset expression
       this.exclamation = "";
     }
   }
